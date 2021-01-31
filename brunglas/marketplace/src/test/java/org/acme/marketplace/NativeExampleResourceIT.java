fc9badb0e5730a3b5dc335b39c65c779bffda0a3
Gomes <jonathan2gomes.jg@gmail.com>
2021-01-30 21:02:50 -0300

    including project structure, setting database as docker-compose

---
+++ brunglas/marketplace/src/test/java/org/acme/marketplace/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.marketplace;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
